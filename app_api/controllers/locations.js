be268d9e7d9368ac5162123f4e709a801b682516
testuser <test@test.com>
2019-11-14 18:36:01 -0500

    err checking distance

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -13,11 +13,16 @@ if (process.env.NODE_ENV === 'production') {
 const formatDistance = (distance) => {
   let thisDistance = 0;
   let unit = 'm';
-  if (distance > 1000) {
-    thisDistance = parseFloat(distance / 1000).toFixed(1);
-    unit = 'km';
+  if(!isNaN(distance)) {
+    if (distance > 1000) {
+      thisDistance = parseFloat(distance / 1000).toFixed(1);
+      unit = 'km';
+    } else {
+      thisDistance = Math.floor(distance);
+    }
   } else {
-    thisDistance = Math.floor(distance);
+    thisDistance = -999;
+    unit = invalid
   }
   return thisDistance + unit;
 }
